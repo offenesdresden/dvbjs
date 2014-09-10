51c3567c9d662a684f14df7e93331a158f0c4647
Astro <astro@spaceboyz.net>
2014-09-10 17:22:15 +0200

    callback(err, data) style
    fixes GH issue #1: Pass errors to callbacks

--- test/test.js
+++ test/test.js
@@ -1,14 +1,21 @@
+var assert = require('assert');
 var dvb = require('../index.js');
 
 describe('dvb.monitor "Postplatz"', function(){
     it('should return an array', function(done){
-        dvb.monitor('postplatz', 0, 5, function(data){
-            if (Array.isArray(data)) done();
+        dvb.monitor('postplatz', 0, 5, function(err, data){
+            assert.ifError(err);
+            assert(Array.isArray(data));
+            done();
         });
     });
     it('should contain all three fields', function(done){
-        dvb.monitor('postplatz', 0, 5, function(data){
-            if (typeof(data[0].line) !== 'undefined' && typeof(data[0].direction) !== 'undefined' && typeof(data[0].arrivaltime) !== 'undefined') done();
+        dvb.monitor('postplatz', 0, 5, function(err, data){
+            assert.ifError(err);
+            assert(data[0].line);
+            assert(data[0].direction);
+            assert(typeof data[0].arrivaltime != 'undefined');
+            done();
         });
     });
 });
@@ -20,21 +27,28 @@ var deparr = 0;
 
 describe('dvb.route "Prager Straße -> Postplatz"', function(){
     it('should return the correct origin and destination', function(done){
-        dvb.route(origin, destination, time, deparr, function(data){
-            if (data.origin === 'Dresden, Prager Straße' && data.destination === 'Dresden, Postplatz') done();
+        dvb.route(origin, destination, time, deparr, function(err, data){
+            assert.ifError(err);
+            assert(data.origin === 'Dresden, Prager Straße');
+            assert(data.destination === 'Dresden, Postplatz');
+            done();
         });
     });
 });
 
 describe('dvb.find "Zellescher Weg"', function(){
     it('should return an array', function(done){
-        dvb.find('zellesch',function(data){
-            if (Array.isArray(data)) done();
+        dvb.find('zellesch',function(err, data){
+            assert.ifError(err);
+            assert(Array.isArray(data));
+            done();
         });
     });
     it('should find the correct stop', function(done){
-        dvb.find('zellesch', function(data){
-            if (data[0].stop === 'Zellescher Weg') done();
+        dvb.find('zellesch', function(err, data){
+            assert.ifError(err);
+            assert(data[0].stop === 'Zellescher Weg');
+            done();
         });
     });
 });
