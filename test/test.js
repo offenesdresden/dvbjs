5d23a479a31910d1601b666475aa6bac5b1a3ac9
hannesrosenbauer <hrosenbauer@avantgarde-labs.de>
2015-06-24 12:58:34 +0200

    Clean up asserts

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ describe('dvb.monitor "Postplatz"', function() {
     function assertTransport(transport) {
         assert(transport.line);
         assert(transport.direction);
-        assert(typeof transport.arrivaltime === 'number');
+        assert.strictEqual('number', typeof transport.arrivaltime);
     }
 
     it('should return an array', function(done) {
@@ -33,7 +33,7 @@ describe('dvb.route "Prager Straße -> Postplatz"', function() {
         assert(trip.departure);
         assert(trip.arrival);
         assert(trip.duration);
-        assert(typeof trip.interchange === 'number');
+        assert.strictEqual('number', typeof trip.interchange);
 
         assert(Array.isArray(trip.nodes));
         trip.nodes.forEach(assertNode);
@@ -44,32 +44,32 @@ describe('dvb.route "Prager Straße -> Postplatz"', function() {
         assert(node.line || node.line === '');
         assert(node.direction || node.direction === '');
 
-        assert(typeof node.departure === 'object');
+        assert.strictEqual('object', typeof node.departure);
         assert(node.departure.stop);
         assert(node.departure.time);
 
         assert(Array.isArray(node.departure.coords));
-        assert(node.departure.coords.length === 2);
+        assert.strictEqual(2, node.departure.coords.length);
 
-        assert(typeof node.arrival === 'object');
+        assert.strictEqual('object', typeof node.arrival);
         assert(node.arrival.stop);
         assert(node.arrival.time);
 
         assert(Array.isArray(node.arrival.coords));
-        assert(node.arrival.coords.length === 2);
+        assert.strictEqual(2, node.arrival.coords.length);
 
         assert(Array.isArray(node.path));
         node.path.forEach(function (path) {
             assert(Array.isArray(path));
-            assert(path.length === 2);
+            assert.strictEqual(2, path.length);
         });
     }
 
     it('should return the correct origin and destination', function(done) {
         dvb.route('pragerstrasse', 'postplatz', new Date(), dvb.route.DEPARTURE, function(err, data) {
             assert.ifError(err);
-            assert(data.origin === 'Dresden, Prager Straße');
-            assert(data.destination === 'Dresden, Postplatz');
+            assert.strictEqual('Dresden, Prager Straße', data.origin);
+            assert.strictEqual('Dresden, Postplatz', data.destination);
             assert(Array.isArray(data.trips));
             data.trips.forEach(assertTrip);
             done();
@@ -81,7 +81,7 @@ describe('dvb.find "Zellescher Weg"', function() {
     function assertStop(stop) {
         assert(stop.stop);
         assert(Array.isArray(stop.coords));
-        assert(stop.coords.length === 2);
+        assert.strictEqual(2, stop.coords.length);
     }
 
     it('should return an array', function(done) {
@@ -97,7 +97,7 @@ describe('dvb.find "Zellescher Weg"', function() {
     it('should find the correct stop', function(done) {
         dvb.find('zellesch', function(err, data) {
             assert.ifError(err);
-            assert(data[0].stop === 'Zellescher Weg');
+            assert.strictEqual('Zellescher Weg', data[0].stop);
             done();
         });
     });
