32629df41f19112543bb296732d7173439745828
Florian Schmidt <florian.schmidt.1994@icloud.com>
2015-07-25 12:47:57 +0200

    Fix

--- test/test.js
+++ test/test.js
@@ -158,7 +158,7 @@ describe('dvb.pins "5654791, 4620310, 5657216, 4623119, stop"', function() {
 describe('dvb.address "51.051487, 13.738256"', function() {
     it('should resolve into an object with city and address properties', function () {
         var data = dvb.address(51.051487, 13.738256)
-        .then(function() {
+        .then(function(data) {
             assert(data.address);
             assert(data.city);
         });
