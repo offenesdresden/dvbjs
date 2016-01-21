e4848a98e61b3a5258bdd1e4a398ebc0f1845253
Andreas Hippler <andreas@tabaro.de>
2016-01-21 20:02:56 +0100

    add type of pin, request multiple types at once

--- test/test.js
+++ test/test.js
@@ -257,13 +257,14 @@ describe('dvb.pins', function () {
                 });
         });
 
-        it('should contain objects with id, name, coords and connections', function (done) {
+        it('should contain objects with id, name, coords, type and connections', function (done) {
             dvb.pins(51.026578, 13.713899, 51.035565, 13.737974, 'stop')
                 .then(function (data) {
                     data.forEach(function (elem) {
                         assert(elem.id);
                         assert(elem.name);
                         assert(elem.coords);
+                        assert(elem.type);
                         assert.strictEqual(2, elem.coords.length);
                         assert.strictEqual(13, Math.floor(elem.coords[1]));
                         assert.strictEqual(51, Math.floor(elem.coords[0]));
@@ -292,13 +293,14 @@ describe('dvb.pins', function () {
     describe('dvb.pins "51.026578, 13.713899, 51.035565, 13.737974, platform"', function () {
         mockRequest('pins-platform.json');
 
-        it('should contain objects with name, coords and platform_nr', function (done) {
+        it('should contain objects with name, coords, type and platform_nr', function (done) {
             dvb.pins(51.026578, 13.713899, 51.035565, 13.737974, 'platform')
                 .then(function (data) {
                     assert.notEqual(0, data.length);
                     data.forEach(function (elem) {
                         assert(elem.name);
                         assert(elem.coords);
+                        assert(elem.type);
                         assert.strictEqual(2, elem.coords.length);
                         assert.strictEqual(13, Math.floor(elem.coords[1]));
                         assert.strictEqual(51, Math.floor(elem.coords[0]));
@@ -315,18 +317,46 @@ describe('dvb.pins', function () {
     describe('dvb.pins "51.026578, 13.713899, 51.035565, 13.737974, POI"', function () {
         mockRequest('pins-poi.json');
 
-        it('should contain objects with name, coords and id', function (done) {
-            dvb.pins(51.026578, 13.713899, 51.035565, 13.737974, dvb.pins.type.POI)
+        it('should contain objects with name, coords, type and id', function (done) {
+            var pinType = dvb.pins.type.POI;
+            dvb.pins(51.026578, 13.713899, 51.035565, 13.737974, pinType)
                 .then(function (data) {
                     assert.notEqual(0, data.length);
                     data.forEach(function (elem) {
                         assert(elem.id);
                         assert(elem.name);
                         assert(elem.coords);
+                        assert(elem.type);
                         assert.strictEqual(2, elem.coords.length);
                         assert.strictEqual(13, Math.floor(elem.coords[1]));
                         assert.strictEqual(51, Math.floor(elem.coords[0]));
                     });
+                    done();
+                })
+                .catch(function (err) {
+                    done(err);
+                });
+        });
+    });
+
+    describe('dvb.pins "51.026578, 13.713899, 51.035565, 13.737974, POI+PLATFORM"', function () {
+        mockRequest('pins-poi-pf.json');
+
+        it('should contain objects with pinType of RENT_A_BIKE or PLATFORM', function (done) {
+            var hasPOI = false, hasSTOP = false;
+
+            dvb.pins(51.026578, 13.713899, 51.035565, 13.737974, [dvb.pins.type.RENT_A_BIKE, dvb.pins.type.PLATFORM])
+                .then(function (data) {
+                    data.forEach(function (elem) {
+                        if (elem.type == dvb.pins.type.RENT_A_BIKE)
+                            hasPOI = true;
+                        else if (elem.type == dvb.pins.type.PLATFORM)
+                            hasSTOP = true;
+                        else
+                            assert.fail(elem.type, dvb.pins.type.RENT_A_BIKE + " || " + dvb.pins.type.PLATFORM, "pinType should be '" + dvb.pins.type.RENT_A_BIKE + " or '" + dvb.pins.type.PLATFORM + "'");
+                    });
+                    assert(hasPOI && hasSTOP, "pinTypes should be '" + dvb.pins.type.RENT_A_BIKE + " and '" + dvb.pins.type.PLATFORM + "'");
+
                     done();
                 })
                 .catch(function (err) {
