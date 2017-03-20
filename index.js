64cdc0f151fd168cbe2b2294cb044c63e16ecdc7
Andreas Hippler <andreas@tabaro.de>
2017-03-20 23:43:05 +0100

    add .findPOI, rename .find to .findStop, address use pointfinder api

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 'use strict';
 
+var find = require('./lib/find');
+
 var dvb = {
-    find: require('./lib/find'),
+    findStop: find.findStop,
+    findPOI: find.findPOI,
+    address: find.findAddress,
     route: require('./lib/route'),
     monitor: require('./lib/monitor'),
     pins: require('./lib/pins'),
-    address: require('./lib/address'),
     coords: require('./lib/coords')
 };
 
 module.exports = dvb;
+// var util = require('util');
+// util.inspect.defaultOptions.maxArrayLength = 3;
+//
+// require('better-log').install({depth: 10});
+
+// dvb.address(51.02836082515807, 13.745858712566005).then(function (data) {
+//     console.log(data);
+// });
+
+
+// dvb.findPOI('Frauenkirche Dresden').then(function (data) {
+//     console.log(data);
+// });
+
+// dvb.findStop('Fehrbellin').then(function (data) {
+//     console.log(data);
+// });
+
+// dvb.route(33000742, 33000037, new Date(), false)
+//     .then(function (data) {
+//         console.log(data)
+//     });
