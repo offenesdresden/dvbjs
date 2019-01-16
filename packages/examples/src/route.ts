914ee6e1c51d03b9a71171e2cf63b114ee56af66
BenReim <benj.reim@gmail.com>
2019-01-16 18:43:32 +0100

    Adapt route sample

--- packages/examples/src/route.ts
+++ packages/examples/src/route.ts
@@ -4,6 +4,6 @@ const origin = "33000742"; // Helmholtzstraße
 const destination = "33000037"; // Postplatz
 const startTime = new Date();
 
-dvb.route(origin, destination, startTime).then((data) => {
+dvb.route(origin, destination, startTime, false).then((data) => {
   console.dir(data, { depth: 7, maxArrayLength: 2 });
 });
