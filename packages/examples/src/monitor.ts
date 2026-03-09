89f118580cbb6dd4eec2b503c41a06d453ce0b79
Kilian Koeltzsch <me@kilian.io>
2026-03-09 19:19:16 +0100

    flatten monorepo and switch to npm

--- packages/examples/src/monitor.ts
+++
@@ -1,9 +0,0 @@
-import * as dvb from "dvbjs"; // OR const dvb = require("dvbjs");
-
-const stopID = "33000037"; // Postplatz
-const timeOffset = 5;
-const numResults = 2;
-
-dvb.monitor(stopID, timeOffset, numResults).then((data) => {
-  console.dir(data, { depth: 7, maxArrayLength: 2 });
-});
